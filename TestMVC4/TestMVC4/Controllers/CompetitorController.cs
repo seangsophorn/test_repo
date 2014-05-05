ba3a4030d63a76f1e91f01971081fe711d5b5a3f
Sophorn <seang_sophorn@yahoo.com>
2014-05-05 15:19:02 +0700

    update

---
+++ TestMVC4/TestMVC4/Controllers/CompetitorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVC4.Controllers
+{
+    public class CompetitorController : Controller
+    {
+        //
+        // GET: /Competitor/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
